cc03ef718edb27d01366650b17ea758b0b97e8de
Lucas Gabriel de Almeida <82488059+Lga2311@users.noreply.github.com>
2021-09-27 20:11:58 -0300

    Add Arquivos

---
+++ DrawObjects/DrawObjects/Class2.vb
@@ -0,0 +1,44 @@
+﻿Imports Autodesk.AutoCAD.ApplicationServices
+Imports Autodesk.AutoCAD.DatabaseServices
+Imports Autodesk.AutoCAD.Runtime
+Imports Autodesk.AutoCAD.EditorInput
+Imports Autodesk.AutoCAD.Geometry
+Imports DrawCAD.CAD14Class
+
+Public Class Class2
+
+    <CommandMethod("ElevarParedes")>
+    Public Sub ElevarParedes()
+        Dim doc As Document = Application.DocumentManager.MdiActiveDocument
+        Dim db As Database = doc.Database
+        Dim edt As Editor = doc.Editor
+
+        Using trans As Transaction = db.TransactionManager.StartTransaction()
+
+            Try
+                Dim bt As BlockTable
+                bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead)
+
+                Dim btr As BlockTableRecord
+                btr = trans.GetObject(bt(BlockTableRecord.ModelSpace), OpenMode.ForWrite)
+
+                edt.WriteMessage("Drawing a Line object" + vbCrLf)
+                Dim pt1 As Point3d = New Point3d(0, 0, 0)
+                Dim pt2 As Point3d = New Point3d(25, 150, 0)
+                Dim ln As Line = DrawLINE(-61.5118, -58.3218, -1.0391, -1.0391)
+
+                btr.AppendEntity(ln)
+                trans.AddNewlyCreatedDBObject(ln, True)
+                trans.Commit()
+
+
+            Catch ex As Exception
+                edt.WriteMessage("Error encountered: " + ex.Message)
+                trans.Abort()
+            End Try
+
+        End Using
+
+
+    End Sub
+End Class
---
+++ DrawObjects/DrawObjects/DrawLine (Higuit).vb
@@ -0,0 +1,43 @@
+﻿Imports Autodesk.AutoCAD.ApplicationServices
+Imports Autodesk.AutoCAD.DatabaseServices
+Imports Autodesk.AutoCAD.Runtime
+Imports Autodesk.AutoCAD.EditorInput
+Imports Autodesk.AutoCAD.Geometry
+
+Public Class Class1
+
+    <CommandMethod("DrawLine")>
+    Public Sub DrawLine()
+        Dim doc As Document = Application.DocumentManager.MdiActiveDocument
+        Dim db As Database = doc.Database
+        Dim edt As Editor = doc.Editor
+
+        Using trans As Transaction = db.TransactionManager.StartTransaction()
+
+            Try
+                Dim bt As BlockTable
+                bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead)
+
+                Dim btr As BlockTableRecord
+                btr = trans.GetObject(bt(BlockTableRecord.ModelSpace), OpenMode.ForWrite)
+
+                edt.WriteMessage("Drawing a Line object" + vbCrLf)
+                Dim pt1 As Point3d = New Point3d(0, 0, 0)
+                Dim pt2 As Point3d = New Point3d(25, 150, 0)
+                Dim ln As Line = New Line(pt1, pt2)
+                ln.ColorIndex = 1
+                btr.AppendEntity(ln)
+                trans.AddNewlyCreatedDBObject(ln, True)
+                trans.Commit()
+
+            Catch ex As Exception
+                edt.WriteMessage("Error encountered: " + ex.Message)
+                trans.Abort()
+
+            End Try
+
+        End Using
+
+    End Sub
+
+End Class
